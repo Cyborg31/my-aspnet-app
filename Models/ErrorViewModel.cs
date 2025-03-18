6bed9a6fb8c9fb9cdf892b967d0436897cb431ee
Sudip Giri <sudeepgiri31@gmail.com>
2025-03-18 16:48:22 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyFirstAspNetApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
